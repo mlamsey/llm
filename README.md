15e9909ad3a73eb1a31cbf51d0344e3b5d55b566
mlamsey <31256490+mlamsey@users.noreply.github.com>
2025-01-28 20:48:04 -0500

    install

---
+++ README.md
@@ -0,0 +1,13 @@
+# LLM Playground
+
+For experimenting with LLMs.
+
+# Installation
+
+Tested with Python 3.12. Use the provided `install.py` script to install `pip` packages depending on system CUDA version.
+
+# Notes
+
+## Qwen
+
+First model for testing. https://huggingface.co/Qwen
---
+++ install.py
@@ -0,0 +1,43 @@
+import os
+
+def install_torch():
+    while True:
+        print("CUDA Options: 11.8, 12.1, 12.4")
+        print("Enter 'q' to quit")
+        ui = input("Choose a CUDA version for torch: ").lower()
+
+        if ui == 'q':
+            return
+
+        if ui == '11.8':
+            index_url = "https://download.pytorch.org/whl/cu118"
+            break
+        elif ui == '12.1':
+            index_url = "https://download.pytorch.org/whl/cu120"
+            break
+        elif ui == '12.4':
+            index_url = "https://download.pytorch.org/whl/cu124"
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+    try:
+        os.system(f"pip install torch torchvision torchaudio -f {index_url}")
+    except:
+        print("Error installing torch. Please try again.")
+
+def install():
+    print("Installing torch")
+    install_torch()
+
+    # other stuff
+    os.system("pip install transformers")
+    os.system("pip install 'accelerate>=0.26.0'")
+    os.system("pip install optimum")
+
+if __name__ == "__main__":
+    ui = input("Enter 'y' to install torch and other dependencies: ").lower()
+    if ui == 'y':
+        install()
+    else:
+        print("Exiting...")
